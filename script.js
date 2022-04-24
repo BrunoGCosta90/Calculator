782cdfacde53c94a017dc9e1fd3c199602da3807
BrunoGCosta90 <brunogallegocosta@hotmail.com>
2022-04-24 14:21:28 -0300

    Changed button collors on style.css Added keyboard support for calculator, fixed bug when chaining operators fixed results overflowing the display

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ grabButton.forEach(button =>
     button.addEventListener('click', () => calculatorEngine(button.textContent))
 );
 
+
+document.addEventListener('keydown', keyPress);
+
 function calculatorEngine(char){
     
     
@@ -47,12 +50,13 @@ function calculatorEngine(char){
         case '1':
         case '0':
             if(displayCurrent.textContent == '0' && char != '0'){ displayCurrent.textContent = ''; }
+            
             if(operator == undefined){
                 if(displayCurrent.textContent.length < 15){
                     displayCurrent.textContent += char;
                 }
             } else {
-                displayOld.textContent = `${x}` + ' ' + `${operator}`;
+                displayOld.textContent = `${x} ${operator}`;
                 if(displayCurrent.textContent.length < 15){
                     displayCurrent.textContent += char;
                     y = displayCurrent.textContent;
@@ -64,6 +68,13 @@ function calculatorEngine(char){
         case '+':
         case '%':
         case '*':
+            checkValidOperation();
+            if(x != undefined && operator != undefined){
+                checkValidOperation();
+                displayOld.textContent = x + ` ${char}`;
+                displayCurrent.textContent = 0;
+                break;
+            } 
             operator = char;
             x = displayCurrent.textContent;
             displayOld.textContent = displayCurrent.textContent + ` ${char}`;
@@ -79,20 +90,8 @@ function calculatorEngine(char){
             };
             break;
         case '=':
-            if(x != '' && y != '' && operator != undefined){
-                x = Number(x);
-                y = Number(y);
-                if((x == 0 || y == 0) && operator == '÷'){
-                    alert("Can't divide by zero!");
-                    break;
-                }
-                displayOld.textContent += ` ${displayCurrent.textContent}`;
-                displayCurrent.textContent = Math.round(operate(operator, x, y) * 100) / 100;
-                x = displayCurrent.textContent;
-                y = '';
-                dot.disable = false;
-                break;
-            }
+            checkValidOperation();
+            break;
         default:
             break;
     }
@@ -101,25 +100,47 @@ function calculatorEngine(char){
     }
 }
 
+function checkValidOperation(){
+    if(x != '' && y != '' && operator != undefined){
+        x = Number(x);
+        y = Number(y);
+        if((x == 0 || y == 0) && operator == '÷'){
+            alert("Can't divide by zero!");
+           return ;
+        }
+        displayOld.textContent += ` ${displayCurrent.textContent}`;
+        x = Math.round(operate(operator, x, y) * 100) / 100;
+        if (x.toString().length > 15){
+            displayCurrent.textContent = x.toPrecision(15);
+        } else {
+            displayCurrent.textContent = x;
+        }
+        //displayCurrent.textContent = x;
+        y = 0;
+        operator = undefined;
+        dot.disable = false;
+    }
+}
+
 function operate(operator, x, y){
   switch(operator){
       case '*':
           return multiply(x,y);
-      break;
+          break;
       case '%':
           return percentage(x,y);
-      break;
+          break;
       case '+':
           return add(x,y);
-      break;
+          break;
       case '-':
           return subtract(x,y);
-      break;
+          break;
       case '÷':
           return divide(x,y);
-      break;      
+          break;      
       default:
-      break;
+          break;
   }
 }
 
@@ -156,4 +177,78 @@ function testForFloat(text){
     dot.disabled = false;
     return false;
     
+}
+
+function keyPress(e){
+    switch(e.keyCode){
+        case 8:
+            calculatorEngine('C');
+            break;
+        case 46:
+            calculatorEngine('AC');
+            break;
+        case 107:
+            calculatorEngine('+');
+            break;
+        case 106:
+            calculatorEngine('*');
+            break;
+        case 109:
+            calculatorEngine('-');
+            break;
+        case 111:
+            calculatorEngine('÷')
+            break;
+        case 16:
+            calculatorEngine('%');
+            break;
+        case 110:
+            calculatorEngine('.');
+            break;
+        case 48:
+        case 96:
+            calculatorEngine('0');
+            break;
+        case 49:
+        case 97:
+            calculatorEngine('1');
+            break;
+        case 50:
+        case 98:
+            calculatorEngine('2');
+            break;
+        case 51:
+        case 99:
+            calculatorEngine('3');
+            break;
+        case 52:
+        case 100:
+            calculatorEngine('4');
+            break;
+        case 53:
+        case 101:
+            calculatorEngine('5');
+            break;
+        case 54:
+        case 102:
+            calculatorEngine('6');
+            break;
+        case 55:
+        case 103:
+            calculatorEngine('7');
+            break;
+        case 56:
+        case 104:
+            calculatorEngine('8');
+            break;
+        case 57:
+        case 105:
+            calculatorEngine('9');
+            break;
+        case 13:
+            calculatorEngine('=');
+            break;
+        default:
+            break;
+    }
 }
